30a0d9f9dee7c0b2bb55c14a06c6dfd53426ae3b
jprolejko <jprolejko@gmail.com>
2019-06-05 21:04:32 +0200

    Utils functions made as inline

--- include/Utils.h
+++ include/Utils.h
@@ -5,20 +5,54 @@
 #ifndef GRAVITY_UTILS_H
 #define GRAVITY_UTILS_H
 
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
 
 #include <Planet.h>
 
 namespace utils {
 
-    sf::Vector2f vectorLengthLimit(const sf::Vector2f &vector_in, float max_length);
+    inline sf::Vector2f vectorLengthLimit(const sf::Vector2f &vector_in, float max_length) {
+        float length = std::hypot(vector_in.x, vector_in.y);
 
-    bool isNearlyEqual(const sf::Vector2f &a, const sf::Vector2f &b, float abs_error);
+        sf::Vector2f out = vector_in;
 
-    double getDistance(const sf::Vector2f &a, const sf::Vector2f &b);
+        if (length > max_length) {
+            out.x = out.x / length * max_length;
+            out.y = out.y / length * max_length;
+        }
 
-    bool isCollidable(const Planet &first, const Planet &second, float time_elapsed);
-    
+        return out;
+    }
+
+    inline bool isNearlyEqual(float a, float b, float abs_error) {
+        return std::abs(a - b) < abs_error;
+    }
+
+    inline bool isNearlyEqual(const sf::Vector2f &a, const sf::Vector2f &b, float abs_error) {
+        return std::abs(a.x - b.x) < abs_error && std::abs(a.y - b.y) < abs_error;
+    }
+
+    inline double getDistance(const sf::Vector2f &a, const sf::Vector2f &b) {
+        return std::hypot(b.x - a.x, b.y - a.y);
+    }
+
+    inline bool isCollidable(const Planet &first, const Planet &second, float time_elapsed) {
+        // circle - circle collision
+        {
+            double distance = utils::getDistance(first.getPosition(), second.getPosition());
+            double max_distance = first.getRadius() + second.getRadius();
+                
+            if (distance < max_distance)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 } // namespace utils
 
 #endif //GRAVITY_UTILS_H
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -9,37 +9,6 @@
 
 namespace utils {
 
-    sf::Vector2f vectorLengthLimit(const sf::Vector2f &vector_in, float max_length) {
-        float length = std::hypot(vector_in.x, vector_in.y);
 
-        sf::Vector2f out = vector_in;
-
-        if (length > max_length) {
-            out.x = out.x / length * max_length;
-            out.y = out.y / length * max_length;
-        }
-
-        return out;
-    }
-
-    bool isNearlyEqual(const sf::Vector2f &a, const sf::Vector2f &b, float abs_error) {
-        return std::fabs(a.x - b.x) < abs_error && std::fabs(a.y - b.y) < abs_error;
-    }
-
-    double getDistance(const sf::Vector2f &a, const sf::Vector2f &b) {
-        return std::hypot(b.x - a.x, b.y - a.y);
-    }
-
-    bool isCollidable(const Planet &first, const Planet &second, float time_elapsed) {
-        double distance = utils::getDistance(first.getPosition(), second.getPosition());
-        double max_distance = first.getRadius() + second.getRadius();
-            
-        if (distance < max_distance)
-        {
-            return true;
-        }
-
-        return false;
-    }
 
 } // namespace utils
